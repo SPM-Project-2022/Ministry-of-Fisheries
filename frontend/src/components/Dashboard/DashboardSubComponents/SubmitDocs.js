c6bd39590ee9430f9392519fb23ecf64f5e94bdd
Chathura Madhusanka <cmadhusanka29@gmail.com>
2022-10-07 17:17:59 +0530

    Dashboard Configuration

---
+++ frontend/src/components/Dashboard/DashboardSubComponents/SubmitDocs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubmitDocs = () => {
+  return <div>SubmitDocs</div>;
+};
+
+export default SubmitDocs;
---
+++ frontend/src/components/Dashboard/DashboardSubComponents/UnOfficialEmps.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UnOfficialEmps = () => {
+  return <div>UnOfficialEmps</div>;
+};
+
+export default UnOfficialEmps;
